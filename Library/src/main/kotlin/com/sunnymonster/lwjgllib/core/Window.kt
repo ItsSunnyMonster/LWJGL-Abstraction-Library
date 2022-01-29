e923b24a74b627b59d7e208129f27de918884fc7
SunnyMonster <68454823+SunnyMonster123@users.noreply.github.com>
2022-01-30 12:32:05 +1300

    Fixed a problem where the viewport won't resize when the window is resized

--- Library/src/main/kotlin/com/sunnymonster/lwjgllib/core/Window.kt
+++ Library/src/main/kotlin/com/sunnymonster/lwjgllib/core/Window.kt
@@ -8,7 +8,6 @@ import org.lwjgl.opengl.GL11.GL_DEPTH_TEST
 import org.lwjgl.opengl.GL11.glEnable
 import org.lwjgl.opengl.GL43.*
 import org.lwjgl.opengl.GLDebugMessageCallback
-import org.lwjgl.opengl.GLDebugMessageCallbackI
 import org.lwjgl.system.MemoryUtil.NULL
 
 /**
@@ -22,46 +21,6 @@ class Window
     private val handle : Long
     private var active : Boolean = true
     private val renderer : Renderer
-
-    @Suppress("NAME_SHADOWING")
-    private val debugCallback : GLDebugMessageCallbackI = GLDebugMessageCallbackI { source, type, id, severity, length, message, userParam ->
-        run {
-
-            val source: String = when (source) {
-                GL_DEBUG_SOURCE_API -> "API"
-                GL_DEBUG_SOURCE_WINDOW_SYSTEM -> "WINDOW SYSTEM"
-                GL_DEBUG_SOURCE_SHADER_COMPILER -> "SHADER COMPILER"
-                GL_DEBUG_SOURCE_THIRD_PARTY -> "THIRD PARTY"
-                GL_DEBUG_SOURCE_APPLICATION -> "APPLICATION"
-                GL_DEBUG_SOURCE_OTHER -> "OTHER"
-                else -> "UNKNOWN"
-            }
-
-            val type: String = when (type) {
-                GL_DEBUG_TYPE_ERROR -> "ERROR"
-                GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR -> "DEPRECATED BEHAVIOR"
-                GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR -> "UNDEFINED BEHAVIOR"
-                GL_DEBUG_TYPE_PORTABILITY -> "PORTABILITY"
-                GL_DEBUG_TYPE_PERFORMANCE -> "PERFORMANCE"
-                GL_DEBUG_TYPE_OTHER -> "OTHER"
-                GL_DEBUG_TYPE_MARKER -> "MARKER"
-                GL_DEBUG_TYPE_PUSH_GROUP -> "PUSH GROUP"
-                GL_DEBUG_TYPE_POP_GROUP -> "POP GROUP"
-                else -> "UNKNOWN"
-            }
-
-            val severity: String = when (severity) {
-                GL_DEBUG_SEVERITY_HIGH -> "HIGH"
-                GL_DEBUG_SEVERITY_MEDIUM -> "MEDIUM"
-                GL_DEBUG_SEVERITY_LOW -> "LOW"
-                GL_DEBUG_SEVERITY_NOTIFICATION -> "NOTIFICATION"
-                else -> "UNKNOWN"
-            }
-
-            println(String.format("%d: %s of %s severity, raised from %s: %s\n",
-                id, type, severity, source, GLDebugMessageCallback.getMessage(length, message)))
-        }
-    }
 
     init {
         GLFWErrorCallback.createPrint(System.err).set()
@@ -88,7 +47,52 @@ class Window
         glEnable(GL_DEBUG_OUTPUT)
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS)
 
-        glDebugMessageCallback(debugCallback, NULL)
+        glDebugMessageCallback({ source, type, id, severity, length, message, _ ->
+            run {
+                @Suppress("NAME_SHADOWING")
+                val source: String = when (source) {
+                    GL_DEBUG_SOURCE_API -> "API"
+                    GL_DEBUG_SOURCE_WINDOW_SYSTEM -> "WINDOW SYSTEM"
+                    GL_DEBUG_SOURCE_SHADER_COMPILER -> "SHADER COMPILER"
+                    GL_DEBUG_SOURCE_THIRD_PARTY -> "THIRD PARTY"
+                    GL_DEBUG_SOURCE_APPLICATION -> "APPLICATION"
+                    GL_DEBUG_SOURCE_OTHER -> "OTHER"
+                    else -> "UNKNOWN"
+                }
+
+                @Suppress("NAME_SHADOWING")
+                val type: String = when (type) {
+                    GL_DEBUG_TYPE_ERROR -> "ERROR"
+                    GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR -> "DEPRECATED BEHAVIOR"
+                    GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR -> "UNDEFINED BEHAVIOR"
+                    GL_DEBUG_TYPE_PORTABILITY -> "PORTABILITY"
+                    GL_DEBUG_TYPE_PERFORMANCE -> "PERFORMANCE"
+                    GL_DEBUG_TYPE_OTHER -> "OTHER"
+                    GL_DEBUG_TYPE_MARKER -> "MARKER"
+                    GL_DEBUG_TYPE_PUSH_GROUP -> "PUSH GROUP"
+                    GL_DEBUG_TYPE_POP_GROUP -> "POP GROUP"
+                    else -> "UNKNOWN"
+                }
+
+                @Suppress("NAME_SHADOWING")
+                val severity: String = when (severity) {
+                    GL_DEBUG_SEVERITY_HIGH -> "HIGH"
+                    GL_DEBUG_SEVERITY_MEDIUM -> "MEDIUM"
+                    GL_DEBUG_SEVERITY_LOW -> "LOW"
+                    GL_DEBUG_SEVERITY_NOTIFICATION -> "NOTIFICATION"
+                    else -> "UNKNOWN"
+                }
+
+                println(String.format("%d: %s of %s severity, raised from %s: %s\n",
+                    id, type, severity, source, GLDebugMessageCallback.getMessage(length, message)))
+            }
+        }, NULL)
+
+        glfwSetFramebufferSizeCallback(handle) { _, width, height ->
+            run {
+                glViewport(0, 0, width, height)
+            }
+        }
 
         renderer = Renderer(this)
 
