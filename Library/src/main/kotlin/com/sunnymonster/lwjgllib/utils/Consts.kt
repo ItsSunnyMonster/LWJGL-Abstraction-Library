1aa63d8bf3cbc24b425c71eab245affe3c6901e3
SunnyMonster <68454823+SunnyMonster123@users.noreply.github.com>
2022-01-30 15:43:29 +1300

    Fixed @JvmStatic in Consts.kt

--- Library/src/main/kotlin/com/sunnymonster/lwjgllib/utils/Consts.kt
+++ Library/src/main/kotlin/com/sunnymonster/lwjgllib/utils/Consts.kt
@@ -2,8 +2,11 @@ package com.sunnymonster.lwjgllib.utils
 
 class Consts {
     companion object {
+        @JvmStatic
         val ROOT = "/"
+        @JvmStatic
         val PACKAGE_ROOT = ROOT + "com/sunnymonster/lwjgllib/"
+        @JvmStatic
         val SHADER_SOURCE_ROOT = PACKAGE_ROOT + "shadersource/"
     }
 }
